8c09bdb7022b8caad0404bf209948373cdf589d0
Otman Bouchari <obouchari@gmail.com>
2016-04-17 01:56:38 -0400

    Add sendRequest function to handle Ajax requests

--- sample/app.js
+++ sample/app.js
@@ -1,11 +1,20 @@
 // Using only http method
-var http = $http({url: 'dfhbdfb'});
+var http = $http({
+	method: 'GET',
+	url: 'http://pokeapi.co/api/v2/pokemon/1/',
+	headers: {
 
-console.log(http.then());
+	}
+});
+console.log(http);
 
 // Using Get
-
-
+// var get = $http.get('http://website.com', {
+// 	headers: {
+// 		'Type': 'Using Get'
+// 	}
+// });
+// console.log(get);
 
 // Using Post
 
--- src/http.js
+++ src/http.js
@@ -3,7 +3,22 @@
     let Promise = require('es6-promise').Promise;
     let _ = {
         isObject: require('lodash.isobject'),
+        isString: require('lodash.isstring'),
         extend: require('lodash.assign')
+    };
+
+    let defaults = {
+        headers: {
+            common: {
+                'Accept': 'application/json, text/plain, */*'
+            },
+            post: {
+                'Content-Type': 'application/json'
+            },
+            put: {
+                'Content-Type': 'application/json'
+            }
+        }
     };
 
     /**
@@ -12,15 +27,111 @@
      * @return {HttpPromise}      Future object
      */
     let http = (requestConfig) => {
+
         if (!_.isObject(requestConfig)) {
             throw 'Http request configuration must be an object.  Received: ' + requestConfig;
         }
-        return {
-            then: function() {
-                return requestConfig;
+
+        if (!_.isString(requestConfig.url) || requestConfig.url === "") {
+            throw 'Http request configuration url must be a string.  Received: ' + requestConfig.url;
+        }
+
+        let config = _.extend({
+            method: 'get',
+        }, requestConfig);
+
+        config.headers = _.extend({}, defaults.headers.common, requestConfig.headers);
+        config.method = config.method.toUpperCase();
+
+        sendRequest(config);
+
+        console.log(config);
+
+        // let serverRequest = function(config) {
+        //     let headers = config.headers;
+        //     let reqData = transformData(config.data, headersGetter(headers), undefined, config.transformRequest);
+
+        //     // strip content-type if data is undefined
+        //     if (isUndefined(reqData)) {
+        //         forEach(headers, (value, header) => {
+        //             if (lowercase(header) === 'content-type') {
+        //                 delete headers[header];
+        //             }
+        //         });
+        //     }
+
+        //     // send request
+        //     return sendReq(config, reqData).then(transformResponse, transformResponse);
+        // };
+
+        // let chain = [serverRequest, undefined];
+        // let promise = $q.when(config);
+
+        // while (chain.length) {
+        //     let thenFn = chain.shift();
+        //     let rejectFn = chain.shift();
+
+        //     promise = promise.then(thenFn, rejectFn);
+        // }
+
+        // return promise;
+
+
+        // function mergeHeaders(config) {
+        //     let defHeaders = defaults.headers,
+        //         reqHeaders = _.extend({}, config.headers),
+        //         defHeaderName, lowercaseDefHeaderName, reqHeaderName;
+
+        //     defHeaders = _.extend({}, defHeaders.common, defHeaders[config.method.toLowerCase()]);
+
+        //     // using for-in instead of forEach to avoid unnecessary iteration after header has been found
+        //     defaultHeadersIteration:
+        //         for (defHeaderName in defHeaders) {
+        //             lowercaseDefHeaderName = defHeaderName.toLowerCase();
+
+        //             for (reqHeaderName in reqHeaders) {
+        //                 if (lowercase(reqHeaderName) === lowercaseDefHeaderName) {
+        //                     continue defaultHeadersIteration;
+        //                 }
+        //             }
+
+        //             reqHeaders[defHeaderName] = defHeaders[defHeaderName];
+        //         }
+
+        //     // execute if header value is a function for merged headers
+        //     return executeHeaderFns(reqHeaders, shallowCopy(config));
+        // }
+    };
+
+    function sendRequest(config) {
+        let request = new XMLHttpRequest();
+        let headers = config.headers;
+
+        request.open(
+            config.method,
+            config.url,
+            true
+        );
+
+        for (let header in headers) {
+            if (headers.hasOwnProperty(header)) {
+                request.setRequestHeader(header, headers[header]);
+            }
+        }
+
+        request.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status >= 200 && this.status < 400) {
+                    console.log(this);
+                } else {
+                    console.log(this);
+                }
             }
         };
-    };
+
+        request.send();
+        request = null;
+    }
 
     _createShortMethods('get', 'delete', 'head', 'jsonp'); // 'get', 'delete', 'head', 'jsonp'
     _createShortMethodsWithData('post', 'put', 'patch'); // 'post', 'put', 'patch'
